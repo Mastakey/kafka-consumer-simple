94bda4f13c4fd4107bbc1ad90b0a59e4d0089a2e
Kioh Han <kioh.han@gmail.com>
2020-07-17 16:21:00 -0400

    initializr

---
+++ src/main/java/com/kioh/simplekafkaconsumer/SimplekafkaconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kioh.simplekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplekafkaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplekafkaconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kioh/simplekafkaconsumer/SimplekafkaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kioh.simplekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplekafkaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
